59a7818f35b85335eda5477eec6232a3f6bfd472
hshobhith <109451746+hshobhith@users.noreply.github.com>
2025-02-15 10:49:58 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-     // baseURL : 'http://localhost:5000'
-    baseURL : 'https://ydemy-clone-api.vercel.app'
+     baseURL : 'http://localhost:5000'
+    // baseURL : 'https://ydemy-clone-api.vercel.app'
 });
 
 axiosInstance.interceptors.request.use(config=> {
